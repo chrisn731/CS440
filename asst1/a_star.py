61bfc8dda0a697609e99f652a78000a73d798f31
Chris Naporlee <chrisn731@gmail.com>
2022-01-31 19:37:10 -0500

    A star working???? No shot.

---
+++ asst1/a_star.py
@@ -0,0 +1,73 @@
+from queue import PriorityQueue
+from graph_ent import Edge
+
+def get_edge(s, e, edges):
+    if (s, e) in edges:
+        return edges[s, e]
+    elif (e, s) in edges:
+        return edges[e, s]
+    else:
+        return None
+
+def heuristic(s, goal):
+    return 0
+
+def update_vertex(s, e, cost_so_far, parent, edge: Edge, fringe: PriorityQueue):
+    new = cost_so_far[s] + edge.weight
+    if new < cost_so_far[e]:
+        cost_so_far[e] = new
+        parent[e] = s
+        fringe.put((cost_so_far[e], e))
+
+def a_star(start, goal, edges):
+    print("Start: " + str(start))
+    print("Goal: " + str(goal))
+    fringe = PriorityQueue() # [(f, (x, y))]
+    closed = []
+    cost_so_far = dict()
+    parent = dict()
+    
+    cost_so_far[start] = 0
+    parent[start] = start
+    fringe.put((0, start))
+
+    while not fringe.empty():
+        t = fringe.get()
+        s = t[1]
+
+        if s == goal:
+            print("Path Found with cost: " + str(t[0]))
+            curr = s
+            p = parent[s]
+            while p != start:
+                print(curr)
+                curr = p
+                p = parent[p]
+            print(curr)
+            print(p)
+            return
+
+        closed.append(s)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                x = s[0]
+                y = s[1]
+                end = (x + i, y + j)
+                if end in closed:
+                    continue
+
+                edge = get_edge(s, end, edges)
+                if not edge:
+                    continue
+
+                is_new = True
+                for element in fringe.queue:
+                    if element[1] == end:
+                        is_new = False
+
+                if is_new:
+                    cost_so_far[end] = float('inf')
+                    parent[end] = None
+                update_vertex(s, end, cost_so_far, parent, edge, fringe)
+
+    print("No path found")
